4d0330899b20555f41197df4d42e1355613c3395
Josh Chase <jcjoshuachase@gmail.com>
2014-09-05 13:50:41 -0400

    migration from http://github.com/Pursuit92/openstack

---
+++ v2/jsontypes.go
@@ -0,0 +1,18 @@
+package image
+
+import (
+	"github.com/Pursuit92/openstack/core"
+)
+
+type Image struct {
+	Id       string            `json:"id,omitempty"`
+	Name     string            `json:"name,omitempty"`
+	Created  string            `json:"created,omitempty"`
+	Updated  string            `json:"updated,omitempty"`
+	Status   string            `json:"status,omitempty"`
+	Progress int               `json:"progress,omitempty"`
+	MinDisk  int               `json:"minDisk,omitempty"`
+	MinRam   int               `json:"minRam,omitempty"`
+	Metadata map[string]string `json:"metadata,omitempty"`
+	Links    []core.Link       `json:"links,omitempty"`
+}
